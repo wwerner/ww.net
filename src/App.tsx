824988ecf72e16b508933849e88872f9462d837d
Wolfgang Werner <mail@wolfgang-werner.net>
2019-06-29 13:38:08 +0200

    Remove sample app cruft

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import WebProfile from "./components/WebProfile"
 import "./App.css";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
@@ -7,23 +6,9 @@ import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div className='icons'>
+      <div className='icons'>
         <WebProfile title='test' icon={faTwitter} url={new URL('https://twitter.com')}></WebProfile>
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 };
