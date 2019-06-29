700f5b042c2560d08cb686af1b7b644f6390c13a
Wolfgang Werner <mail@wolfgang-werner.net>
2019-06-29 23:31:55 +0200

    Toggle rainbow animation based on state

--- src/components/WebProfile.tsx
+++ src/components/WebProfile.tsx
@@ -9,12 +9,21 @@ interface WebProfileProps {
   icon: string;
 }
 
-class WebProfile extends Component<WebProfileProps> {
+interface WebProfileState {
+  isRainbowy: boolean;
+}
+
+class WebProfile extends Component<WebProfileProps, WebProfileState> {
+  constructor(props: WebProfileProps) {
+    super(props);
+    this.state = {isRainbowy: false};
+  }
+
   render() {
     const { title, icon, url } = this.props;
     return (
         <a href={url.href} title={title} target="_blank" rel="noopener noreferrer">
-          <i className={icon + ' fa-fw rainbow'} ></i>
+          <i className={icon + ' fa-fw' + ( this.state.isRainbowy ? ' rainbow' : '') } ></i>
         </a>
     );
   }
